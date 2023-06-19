b146d929d65f097275d4f155933246f0a4f8442e
CW <chiawei1@uw.edu>
2023-06-19 16:23:38 -0700

    modified:   0. Python Usage Notes/Linked List.py 	renamed:    5. Linked List/0. Debugger copy/Debugger.cpp -> 5. Linked List/0. Debugger/Debugger.cpp 	renamed:    5. Linked List/0. Debugger copy/Debugger.py -> 5. Linked List/0. Debugger/Debugger.py 	renamed:    5. Linked List/0. Debugger copy/Easy 206. Reverse Linked List/Reverse Linked List.py -> 5. Linked List/Easy 206. Reverse Linked List/Reverse Linked List.py 	new file:   5. Linked List/Easy 21. Merge Two Sorted Lists/Merge Two Sorted Lists.py

--- 0. Python Usage Notes/Linked List.py
+++ 0. Python Usage Notes/Linked List.py
@@ -4,6 +4,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -19,14 +20,12 @@ class LinkedList:
                 current = current.next
             current.next = new_node
 
-
     # Traverse the Linked List
     def display(self):
         current = self.head
         while current is not None:
             print(current.data)
             current = current.next
-
 
     # Search for an Element in the Linked List
     def search(self, target):
@@ -36,7 +35,6 @@ class LinkedList:
                 return True
             current = current.next
         return False
-
 
     # Remove an Element from the Linked List
     # NOTE: The following method does not work because "current_node = current_node.next"
@@ -64,7 +62,6 @@ class LinkedList:
                 return
             current = current.next
 
-
     # Get the length of the Linked List
     def length(self):
         count = 0
@@ -73,7 +70,6 @@ class LinkedList:
             count += 1
             current = current.next
         return count
-
 
     # Reverse a Linked List
     def reverse(self):
@@ -85,81 +81,6 @@ class LinkedList:
             previous = current
             current = next_node
         self.head = previous
-
-
-# [Practice Makes You a Pro!]
-
-# Create a Linked List
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    # Add element to the Linked List
-    def add(self, data):
-        new_node = Node(data)
-        if self.head == None:
-            self.head = new_node
-        else:
-            current = self.head
-            while current != None:
-                current = current.next
-            current = new_node
-
-    # Traverse the Linked List
-    def display(self):
-        current_node = self.head
-        while current_node != None:
-            print(current_node.data)
-            current_node = current_node.next
-    
-    # Search for an Element in the Linked List
-    def search(self, target):
-        current_node = self.head
-        while current_node != None:
-            if current_node.data == target:
-                return True
-            current_node = current_node.next
-        return False
-
-    # Remove an Element from the Linked List
-    def remove(self, target):
-        if self.head == None:
-            return
-        if self.head.data == target:
-            self.head = self.head.next
-        current_node = self.head
-        while current_node.next != None:
-            if current_node.next.data == target:
-                current_node.next = current_node.next.next
-                return
-            current_node = current_node.next
-
-
-    # Get the length of the Linked List
-    def length(self):
-        count = 0
-        current = self.head
-        while current != None:
-            count += 1
-            current = current.next
-        return
-
-    # Reverse a Linked List
-    def reverse(self):
-        previous = None
-        current = self.head
-        while current != None:
-            next_node = current.next
-            current.next = None
-            previous = current
-            current = next_node
-        self.head = previous
-
 
 
 # [Practice Makes You a Pro!]
